7b40474e1910c23b657fcbb7a4dc4fc9abb0c84d
jasoy <jasoy@192.168.22.11>
2025-12-11 14:54:32 +0900

    Merge branch 'main' of https://github.com/Jooseungho/HK_TEAM_Project.git into main

--- his_system/src/main/java/com/his/system/nurse/NurseController.java
+++ his_system/src/main/java/com/his/system/nurse/NurseController.java
@@ -1,31 +1,31 @@
-package com.his.system.nurse;
-
-import org.springframework.web.bind.annotation.*;
-import lombok.RequiredArgsConstructor;
-
-import com.his.system.patient.Patient;
-import com.his.system.patient.PatientService;
-
-import com.his.system.visit.Visit;
-import com.his.system.visit.VisitService;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/api/nurse")
-public class NurseController {
-
-    private final PatientService patientService;
-    private final VisitService visitService;
-
-    // 환자 등록
-    @PostMapping("/patient/register")
-    public Patient registerPatient(@RequestBody Patient patient) {
-        return patientService.register(patient);
-    }
-
-    // 방문(내원) 등록
-    @PostMapping("/visit/register/{patientId}")
-    public Visit registerVisit(@PathVariable Long patientId) {
-        return visitService.registerVisit(patientId);
-    }
-}
+/*
+ * package com.his.system.nurse;
+ * 
+ * import org.springframework.web.bind.annotation.*; import
+ * lombok.RequiredArgsConstructor;
+ * 
+ * import com.his.system.patient.Patient; import
+ * com.his.system.patient.PatientService;
+ * 
+ * import com.his.system.visit.Visit; import com.his.system.visit.VisitService;
+ * 
+ * @RestController
+ * 
+ * @RequiredArgsConstructor
+ * 
+ * @RequestMapping("/api/nurse") public class NurseController {
+ * 
+ * private final PatientService patientService; private final VisitService
+ * visitService;
+ * 
+ * // 환자 등록
+ * 
+ * @PostMapping("/patient/register") public Patient registerPatient(@RequestBody
+ * Patient patient) { return patientService.register(patient); }
+ * 
+ * // 방문(내원) 등록
+ * 
+ * @PostMapping("/visit/register/{patientId}") public Visit
+ * registerVisit(@PathVariable Long patientId) { return
+ * visitService.registerVisit(patientId); } }
+ */
